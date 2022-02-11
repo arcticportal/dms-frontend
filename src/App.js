d9896e955d16775fa6976c50f55b4535643174bb
mateuszmmi <matheuszmmi@gmail.com>
2022-02-11 15:57:08 +0000

    add: apollo, ol and basic map render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from} from "@apollo/client";
+
+// react
+import React, { useState, useEffect } from 'react';
+
+// openlayers
+import GeoJSON from 'ol/format/GeoJSON'
+// import Feature from 'ol/Feature';
+
+// components
+import MapWrapper from './components/MapWrapper'
+
 
 function App() {
+  
+  // set initial state
+  const [ features, setFeatures ] = useState([])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <MapWrapper features={features} />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
