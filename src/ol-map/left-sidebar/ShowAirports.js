f9104c88e284160817598f5d5c9aa046ae94eb09
mateuszmmi <matheuszmmi@gmail.com>
2022-03-09 14:15:17 +0000

    simplyfy show airports

--- src/ol-map/left-sidebar/ShowAirports.js
+++ src/ol-map/left-sidebar/ShowAirports.js
@@ -30,42 +30,31 @@ const ShowAirports = () => {
 	const { map } = useContext(MapContext)
 	const { error, loading, data } = useQuery(LOAD_AIRPORTS)
 	const [checked, setChecked] = useState(false)
-	const [representation, setRepresentation] = useState({})
+	const [vectorLayer, setVectorLayer] = useState({})
 
-	function addLayer(map, lay) {
-		map.addLayer(lay); lay.setZIndex(10)
-		return () => { if (map) { map.removeLayer(lay); console.log('cleanup')} }
-	}
 
-	useEffect(() => {  // update screen when check toggled
-		if (!map) return
-		var lay = representation.vectorLayer
-		if (!checked) { return () => { console.log('hello')}}
-		return addLayer(map, lay)
-	}, [checked])
-
-	useEffect(() => {  // update screen when repr changed
+	useEffect(() => {  // update screen when representation or check changed
 		if (!map || !checked) return
-		var prev = representation.previousLayer
-		if (prev) map.removeLayer(prev)
-		return addLayer(map, representation.vectorLayer)
-		// return () => {
-		// 	if (map) {
-		// 		map.removeLayer(prev)
-		// 	}
-		// }
-	}, [representation])
+		map.addLayer(vectorLayer)
+		vectorLayer.setZIndex(10)
+		console.log('addLayer')
+		return () => {
+			if (map) {
+				map.removeLayer(vectorLayer);
+				console.log('cleanup')
+			}
+		}
+	}, [vectorLayer, checked])
 
 	useEffect(() => {  // update representation
 		if (!data) return
-		setRepresentation({
-			previousLayer: representation.vectorLayer,
-			vectorLayer: new OLVectorLayer({
+		setVectorLayer(
+			new OLVectorLayer({
 				source: new OLVectorSource({
 					features: addMarkers(convertData(data.airports))
 				})
 			})
-		})
+		)
 	}, [data])
 
 	if (loading) return <p>Loading...</p>;
@@ -82,59 +71,5 @@ const ShowAirports = () => {
 		</>
 	);
 }
-
-// const ShowAirports = () => {
-// 	const { map } = useContext(MapContext);
-
-// 	const { error, loading, data } = useQuery(LOAD_AIRPORTS)
-// 	const [airports, setAirports] = useState([]);
-// 	const [features, setFeatures] = useState([]);
-// 	const [checked, setChecked] = useState(false);
-// 	console.log(data)
-
-
-// 	useEffect(() => {
-// 		if (data && checked) {
-// 			setAirports(convertData(data.airports))
-// 		}
-// 	}, [data, checked])
-
-// 	useEffect(() => {
-// 		if (airports.length) {
-// 			setFeatures(addMarkers(airports))
-// 		}
-// 	}, [airports])
-
-// 	useEffect(() => {
-// 		let vectorLayer = {}
-// 		if (!map) return;
-// 		if (checked) {
-// 			vectorLayer = new OLVectorLayer({
-// 				source: new OLVectorSource({ features }),
-// 			});
-// 			map.addLayer(vectorLayer);
-// 			vectorLayer.setZIndex(10);
-// 		}
-// 		return () => {
-// 			if (map) {
-// 				map.removeLayer(vectorLayer);
-// 			}
-// 		};
-// 	}, [checked, features]);
-
-// 	if (loading) return <p>Loading...</p>;
-// 	if (error) return <p>Error :(</p>;
-// 	return (
-// 		<>
-// 			<input
-// 				type="checkbox"
-// 				checked={checked}
-// 				onChange={e => setChecked(e.target.checked)}
-// 			/>
-// 			{" "}
-// 			Iceland airports
-// 		</>
-// 	);
-// };
 
 export default ShowAirports;
